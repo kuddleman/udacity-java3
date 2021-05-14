87a8800a6a25b230f88617b45348cdb9925f1bf0
Kuddleman <edl1155@gmail.com>
2021-05-14 13:35:59 -0700

    initial commit

---
+++ src/polymorphismExample/Boat.java
@@ -0,0 +1,9 @@
+package polymorphismExample;
+
+public class Boat extends Vehicle {
+    public Boat() {
+        super("Boat Start", "Boat stop",
+                "Boat speed", "Boat Direction");
+    }
+
+}
---
+++ src/polymorphismExample/Car.java
@@ -0,0 +1,10 @@
+package polymorphismExample;
+
+public class Car extends Vehicle {
+    public Car(){
+        super("Car Start", "Car stop",
+                "Car speed", "Car direction");
+    }
+
+
+}
